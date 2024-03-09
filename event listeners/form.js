b5ed7ef60c1a2984a344e2e7eb617797339f8e38
adakamahesh <adakamahesh@gmail.com>
2024-03-09 22:48:12 +0530

    modified event listeners

--- event listeners/form.js
+++ event listeners/form.js
@@ -1,6 +1,5 @@
 var btn=document.getElementById("submit")
 btn.addEventListener('click',sub)
-const password=document.getElementById('password');
 function sub(){
     const email=document.getElementById('username');
     console.log(email.value);
--- event listeners/onclick.js
+++ event listeners/onclick.js
@@ -1,4 +1,6 @@
 function change(){
-    const mail=document.getElementById('email');
-    console.log(mail.value)
+    const firstname=document.getElementById('firstname');
+    // console.log(firstname.value)
+    const lastname=document.getElementById('lastname');  
+    console.log(firstname.value + lastname.value)  
 }
